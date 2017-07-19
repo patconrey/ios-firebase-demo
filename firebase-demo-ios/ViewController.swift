aef0e51959810f54b763300e37afd20efe452226
Patrick Conrey <patrickconrey@gmail.com>
2017-07-18 22:25:41 -0400

    Initial Commit

---
+++ firebase-demo-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebase-demo-ios
+//
+//  Created by Patrick Conrey on 7/18/17.
+//  Copyright © 2017 Conrey Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
